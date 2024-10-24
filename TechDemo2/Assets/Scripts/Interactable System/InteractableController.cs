11ca3f1b2b21a07bc4d0fe0c0130302d92bef5f8
DM <dommcneill@outlook.com>
2024-10-24 23:07:21 +0100

    better interaction checks

--- TechDemo2/Assets/Scripts/Interactable System/InteractableController.cs
+++ TechDemo2/Assets/Scripts/Interactable System/InteractableController.cs
@@ -5,123 +5,153 @@ using UnityEngine;
 
 public class InteractableController : MonoBehaviour
 {
-    public LayerMask IgnoredLayers;
+	public LayerMask IgnoredLayers;
 
-    public float MaxRange = 3f;
+	public float MaxRange = 3f;
 
-    public float MinAllignmentOfRay = 0.8f;
+	public float MinAllignmentOfRay = 0.8f;
 
-    private Camera _camera;
+	private Camera _camera;
+	private Transform _cameraTransform;
 
-    private IInteractable _selectedObject;
+	private IInteractable _selectedObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _camera = Camera.main;
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		_camera = Camera.main;
+		_cameraTransform = _camera.transform;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        GetNearInteractableObjects();
+	// Update is called once per frame
+	void Update()
+	{
+		GetNearInteractableObjects();
 
-        if (Input.GetKeyDown(KeyCode.E) && _selectedObject != null)
-        {
-            _selectedObject.InteractWithObject();
-        }
-    }
+		if (Input.GetKeyDown(KeyCode.E) && _selectedObject != null)
+		{
+			_selectedObject.InteractWithObject();
+		}
+	}
 
-    private void GetNearInteractableObjects()
-    {
-        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+	private void GetNearInteractableObjects()
+	{
+		//Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+		Ray ray = new Ray(_cameraTransform.position, _cameraTransform.forward);
+		Debug.DrawRay(ray.origin, ray.direction, Color.red);
 
-        Collider[] nearObjects = Physics.OverlapSphere(transform.position, MaxRange, ~IgnoredLayers, QueryTriggerInteraction.Ignore);
+		Collider[] nearObjects = Physics.OverlapSphere(transform.position, MaxRange, ~IgnoredLayers, QueryTriggerInteraction.Ignore);
 
-        if (nearObjects.Length <= 0)
-        {
-            DeselectSelectedObject();
+		if (nearObjects.Length <= 0)
+		{
+			DeselectSelectedObject();
 
-            return;
-        }
+			return;
+		}
 
-        List<Collider> objectsNearRay = new List<Collider>();
-
-        for (int i = 0; i < nearObjects.Length; i++)
-        {
-            if (Vector3.Dot(ray.direction, (nearObjects[i].transform.position - transform.position)) > MinAllignmentOfRay)
-            {
-                objectsNearRay.Add(nearObjects[i]);
-            }
-        }
-
-        if (objectsNearRay.Count <= 0)
-        {
-            DeselectSelectedObject();
-
-            return;
-        }
+		// List<Collider> objectsNearRay = new List<Collider>();
 
 
-        if (objectsNearRay.Count == 1)
-        {
-            SelectObject(objectsNearRay[0].transform);
+		Collider closetsObject = null;
+		float Angle = MinAllignmentOfRay;
+		float distanceOfClosestObject = MaxRange;
 
-            return;
-        }
+		for (int i = 0; i < nearObjects.Length; i++)
+		{
+			print(Vector3.Dot(ray.direction.normalized, (nearObjects[i].transform.position - _cameraTransform.position).normalized) + $" > {Angle} | " + Vector3.Distance(_cameraTransform.position, nearObjects[i].transform.position) + $" < {distanceOfClosestObject}");
 
-        Collider closetsObject = null;
-        float distanceOfClosestObject = MaxRange;
 
-        for (int i = 0; i < objectsNearRay.Count; i++)
-        {
+			if (Vector3.Dot(ray.direction.normalized, (nearObjects[i].transform.position - _cameraTransform.position).normalized) > Angle && Vector3.Distance(_cameraTransform.position, nearObjects[i].transform.position) < distanceOfClosestObject && closetsObject == null)
+			{
+				closetsObject = nearObjects[i];
+				distanceOfClosestObject = Vector3.Distance(_cameraTransform.position, nearObjects[i].transform.position);
 
-            if (closetsObject == null)
-            {
-                closetsObject = objectsNearRay[i];
-                distanceOfClosestObject = Vector3.Distance(transform.position, objectsNearRay[i].transform.position);
-                continue;
-            }
+				Angle = Vector3.Dot(ray.direction.normalized, (nearObjects[i].transform.position - _cameraTransform.position).normalized);
 
-            if (Vector3.Distance(transform.position, closetsObject.transform.position) < distanceOfClosestObject)
-            {
-                closetsObject = objectsNearRay[i];
-                distanceOfClosestObject = Vector3.Distance(transform.position, objectsNearRay[i].transform.position);
-                continue;
-            }
-        }
+				continue;
+			}
+			else if (Vector3.Dot(ray.direction.normalized, (nearObjects[i].transform.position - _cameraTransform.position).normalized) > Angle && Vector3.Distance(_cameraTransform.position, nearObjects[i].transform.position) < distanceOfClosestObject)
+			{
 
-        if (closetsObject == null)
-        {
 
-            DeselectSelectedObject();
+				closetsObject = nearObjects[i];
+				distanceOfClosestObject = Vector3.Distance(_cameraTransform.position, nearObjects[i].transform.position);
 
-            return;
-        }
+				Angle = Vector3.Dot(ray.direction.normalized, (nearObjects[i].transform.position - _cameraTransform.position).normalized);
 
-        SelectObject(closetsObject.transform);
-    }
+				continue;
 
-    private void SelectObject(Transform objectToSelect)
-    {
-        if (_selectedObject != null)
-        {
-            DeselectSelectedObject();
-        }
 
-        if (objectToSelect.GetComponent<IInteractable>() == null) return;
+				//objectsNearRay.Add(nearObjects[i]);
+			}
+		}
 
-        _selectedObject = objectToSelect.GetComponent<IInteractable>();
+		// if (objectsNearRay.Count <= 0)
+		// {
+		// 	DeselectSelectedObject();
 
-        _selectedObject.OnSelected();
-    }
+		// 	return;
+		// }
 
-    private void DeselectSelectedObject()
-    {
-        if (_selectedObject == null) return;
 
-        _selectedObject.OnDeselected();
+		// if (objectsNearRay.Count == 1)
+		// {
+		// 	SelectObject(objectsNearRay[0].transform);
 
-        _selectedObject = null;
-    }
+		// 	return;
+		// }
+
+
+
+		// for (int i = 0; i < objectsNearRay.Count; i++)
+		// {
+
+		// 	if (closetsObject == null)
+		// 	{
+		// 		closetsObject = objectsNearRay[i];
+		// 		distanceOfClosestObject = Vector3.Distance(transform.position, objectsNearRay[i].transform.position);
+		// 		continue;
+		// 	}
+
+		// 	if (Vector3.Distance(transform.position, closetsObject.transform.position) < distanceOfClosestObject)
+		// 	{
+		// 		closetsObject = objectsNearRay[i];
+		// 		distanceOfClosestObject = Vector3.Distance(transform.position, objectsNearRay[i].transform.position);
+		// 		continue;
+		// 	}
+		// }
+
+		if (closetsObject == null)
+		{
+
+			DeselectSelectedObject();
+
+			return;
+		}
+
+		SelectObject(closetsObject.transform);
+	}
+
+	private void SelectObject(Transform objectToSelect)
+	{
+		if (_selectedObject != null)
+		{
+			DeselectSelectedObject();
+		}
+
+		if (objectToSelect.GetComponent<IInteractable>() == null) return;
+
+		_selectedObject = objectToSelect.GetComponent<IInteractable>();
+
+		_selectedObject.OnSelected();
+	}
+
+	private void DeselectSelectedObject()
+	{
+		if (_selectedObject == null) return;
+
+		_selectedObject.OnDeselected();
+
+		_selectedObject = null;
+	}
 }
