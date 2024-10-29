c1e25b4578df66b7c255ef650cb65e2cd2e25a39
DM <dommcneill@outlook.com>
2024-10-29 23:58:11 +0000

    ui slots, helmet and other bits

--- TechDemo2/Assets/Scripts/Movement and Look/LookController.cs
+++ TechDemo2/Assets/Scripts/Movement and Look/LookController.cs
@@ -10,6 +10,8 @@ public class LookController : MonoBehaviour
 
 	public Transform CameraHolder;
 
+	public GameObject Helmet;
+
 	public float MaxViewingAngle = 80;
 
 	float _xRotation;
@@ -23,7 +25,15 @@ public class LookController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (Locked) return;
+		if (Locked)
+		{
+			Helmet.SetActive(false);
+			return;
+		}
+		else
+		{
+			Helmet.SetActive(true);
+		}
 
 		HandleLook();
 	}
@@ -36,7 +46,7 @@ public class LookController : MonoBehaviour
 
 		_xRotation -= mouseInputY * Sensitivity;
 
-		Mathf.Clamp(_xRotation, -MaxViewingAngle, MaxViewingAngle);
+		_xRotation = Mathf.Clamp(_xRotation, -MaxViewingAngle, MaxViewingAngle);
 
 
 		CameraHolder.transform.localRotation = Quaternion.Euler(_xRotation, 0, 0);
